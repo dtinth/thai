84f3c682c3b9014db0861bfbad16a6e732abe958
dtinth on MBP M1 <dtinth@spacet.me>
2024-10-10 22:13:58 +0700

    fix vers

--- sql-storage/integration.test.ts
+++ sql-storage/integration.test.ts
@@ -1,11 +1,13 @@
 import { execSync } from "node:child_process";
 
 Deno.test("bun", async () => {
+  execSync("mise install", { stdio: "inherit", cwd: "tests-bun" });
   execSync("mise exec -- bun install", { stdio: "inherit", cwd: "tests-bun" });
   execSync("mise exec -- bun test", { stdio: "inherit", cwd: "tests-bun" });
 });
 
 Deno.test("node", async () => {
+  execSync("mise install", { stdio: "inherit", cwd: "tests-node" });
   execSync("mise exec -- pnpm install", {
     stdio: "inherit",
     cwd: "tests-node",
